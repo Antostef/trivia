a49c554901e58e01a19f0e15fce6d33838fe2420
Antone Stefanaggi <antone@Antones-iMac.local>
2022-05-01 20:45:33 +0200

    separate game engine and game code

--- python3/trivia.py
+++ python3/trivia.py
@@ -141,25 +141,3 @@ class Game:
 
     def _did_player_win(self):
         return not (self.purses[self.current_player] == 6)
-
-
-from random import randrange
-
-if __name__ == '__main__':
-    not_a_winner = False
-
-    game = Game()
-
-    game.add('Chet')
-    game.add('Pat')
-    game.add('Sue')
-
-    while True:
-        game.roll(randrange(5) + 1)
-
-        if randrange(9) == 7:
-            not_a_winner = game.wrong_answer()
-        else:
-            not_a_winner = game.was_correctly_answered()
-
-        if not not_a_winner: break
