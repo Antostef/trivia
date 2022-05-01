5de388f35c597a5d231cf2bc55987be17d22060d
Antone Stefanaggi <antone@Antones-iMac.local>
2022-05-01 20:50:08 +0200

    refactor question creation

--- python3/trivia.py
+++ python3/trivia.py
@@ -16,13 +16,13 @@ class Game:
         self.is_getting_out_of_penalty_box = False
 
         for i in range(50):
-            self.pop_questions.append("Pop Question %s" % i)
-            self.science_questions.append("Science Question %s" % i)
-            self.sports_questions.append("Sports Question %s" % i)
-            self.rock_questions.append(self.create_rock_question(i))
+            self.pop_questions.append(self.create_question(genre='Pop', index=i))
+            self.science_questions.append(self.create_question(genre='Science', index=i))
+            self.sports_questions.append(self.create_question(genre='Sports', index=i))
+            self.rock_questions.append(self.create_question(genre='Rock', index=i))
 
-    def create_rock_question(self, index):
-        return "Rock Question %s" % index
+    def create_question(self, genre: str, index: int):
+        return f"{genre} Question {index}"
 
     def is_playable(self):
         return self.how_many_players >= 2
